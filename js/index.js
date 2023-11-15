e1223086b31d8aaf868631cc95e8b570a221c63b
BiaTrivillin <biatrivillin.code@gmail.com>
2023-11-14 21:48:32 -0300

    created page 2 html and css with gooey animation

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
-const button = document.querySelector('#text__button');
+const buttonTxt = document.querySelector('#text__button');
+const buttonNxt = document.querySelector('#next__button');
 const wave = document.querySelector('#water__wave')
 const text = document.querySelector('.content')
-const buttonStateTwo = document.querySelector('#state__two__button')
+// const buttonStateTwo = document.querySelector('#state__two__button')
 
-button.addEventListener('click', textReveal);
-buttonStateTwo.addEventListener('click', textReveal);
+buttonTxt.addEventListener('click', textReveal);
+// buttonStateTwo.addEventListener('click', textReveal);
+
+function changePage () {
+    window.location.href = '../html/index.html'
+}
 
 function textReveal () {
+
     wave.animate(
         [
             { transform: 'translateX(25vw)'},
@@ -20,9 +26,22 @@ function textReveal () {
         },
     )
 
-    button.animate(
+    buttonTxt.animate(
         [   
-            { transform: 'translateX(-19vw)'},
+            { opacity: 0},
+        ],
+
+        {
+            duration: 100,
+            iterations: 1,
+            fill: 'forwards',
+            easing: 'ease-in-out'
+        },
+    )
+
+    buttonNxt.animate(
+        [   
+            { transform: 'translateX(-20vw)'},
         ],
 
         {
