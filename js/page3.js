22ef0a096f8a19fb2f7ff85e24bfa3887d4b9b83
BiaTrivillin <biatrivillin.code@gmail.com>
2023-11-17 13:46:58 -0300

    created animation showTextTwo

--- js/page3.js
+++ js/page3.js
@@ -29,3 +29,53 @@ function changeImage() {
             delay: 500,
         },)
 }
+
+const animationButton = document.querySelector('#animation__button');
+const textOne = document.querySelector('.text__one')
+const image = document.querySelector('.image__container')
+const textTwo = document.querySelector('.text__two__container')
+
+animationButton.addEventListener('click', showTextTwo);
+
+function showTextTwo() {
+    animationButton.style.display = 'none';
+    textTwo.style.display = 'flex'
+
+    textOne.animate(
+        [
+            { opacity: 0 },
+        ],
+
+        {
+            duration: 1000,
+            iterations: 1,
+            fill: 'forwards',
+            easing: 'ease-in-out'
+        },)
+
+    image.animate(
+        [
+             { transform: 'translateX(53vw)' },
+         ],
+    
+        {
+            duration: 3000,
+             iterations: 1,
+            fill: 'forwards',
+            easing: 'ease-in-out'
+         },)
+
+    textTwo.animate(
+       [
+           { opacity: 1 },
+        ],
+
+       {
+           duration: 3000,
+           iterations: 1,
+           fill: 'forwards',
+           easing: 'ease-in-out',
+           delay: 1500,
+       },)
+
+}
